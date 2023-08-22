8d59c8131a929d9bf3cca92a547f8741dd988b6a
Peter Ng <ngy@vaniercollege.qc.ca>
2023-08-22 09:44:12 -0400

    initial demo for pinia

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ counter.count }}</p>
+    <button @click="counter.increment()">Add</button>
+    <button @click="counter.decrement()">Remove</button>
+    <button @click="custom">Custom</button>
   </div>
 </template>
+
+<script setup>
+import { useCounterStore } from "@/store/counter";
+
+const counter = useCounterStore();
+</script>
