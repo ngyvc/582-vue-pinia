9b13208caeed6da52c9afd219706a164f13cc390
Peter Ng <ngy@vaniercollege.qc.ca>
2023-08-29 11:32:57 -0400

    add public path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/web-framework/demo-build/", //https://router.vuejs.org/guide/essentials/history-mode.html#Example-Server-Configurations
 });
